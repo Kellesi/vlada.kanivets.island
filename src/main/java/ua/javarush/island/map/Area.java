e7ddcdc75df715fdc8e8e9c937d047b80569a807
Vladyslava Kanivets <kanivetsvlada21@gmail.com>
2023-10-26 18:45:30 +0300

    Area & Island update

--- src/main/java/ua/javarush/island/map/Area.java
+++ src/main/java/ua/javarush/island/map/Area.java
@@ -3,18 +3,18 @@ package ua.javarush.island.map;
 import ua.javarush.island.creature.animal.Animal;
 import ua.javarush.island.creature.plant.Plant;
 
-import java.util.Collections;
+import java.util.List;
 import java.util.Map;
-import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Area {
     private final int pointX;
     private final int pointY;
-    private Map<Class<? super Animal>, Set<? super Animal>> residents= Collections.EMPTY_MAP;
-    private Map<Class<? super Plant>, Set<? super Plant>> plants= Collections.EMPTY_MAP;
+    private List<? super Animal> residents;
+    private List<? super Plant> plants;
 
-    public Area(Map<Class<? super Animal>, Set<? super Animal>> residents,
-                Map<Class<? super Plant>, Set<? super Plant>> plants,
+    public Area(List<? super Animal> residents,
+                List<? super Plant> plants,
                 int pointX, int pointY) {
         this.residents = residents;
         this.plants = plants;
@@ -24,5 +24,29 @@ public class Area {
     public Area(int pointX, int pointY) {
         this.pointX=pointX;
         this.pointY=pointY;
+    }
+    public void setResidents(List<? super Animal> residents){
+        this.residents = residents;
+    }
+    public void setPlants(List<? super Plant> plants){
+        this.plants = plants;
+    }
+    public List<? super Animal> getAllResidents(){
+        return residents;
+    }
+    public List<Animal> getResidents(String animalType){
+        return residents.stream().map(Animal.class::cast).filter(animal -> animal.getType().equals(animalType)).toList();
+    }
+    public List<Plant> getPlants(String plantType){
+        return plants.stream().map(Plant.class::cast).filter(plant -> plant.getType().equals(plantType)).toList();
+    }
+    public <T extends Animal>void addResident(T animal){
+        residents.add(animal);
+    }
+    public String getName(){
+        return String.format("Area %d-%d",pointX,pointY);
+    }
+    public Map<String, Long> getCurrentPopulation(){
+        return residents.stream().map(Animal.class::cast).collect(Collectors.groupingBy(Animal::getType, Collectors.counting()));
     }
 }
--- src/main/java/ua/javarush/island/map/Island.java
+++ src/main/java/ua/javarush/island/map/Island.java
@@ -1,14 +1,30 @@
 package ua.javarush.island.map;
 
+import ua.javarush.island.creature.AnimalType;
+import ua.javarush.island.creature.PlantType;
+import ua.javarush.island.worldgenerator.CreatureFactory;
+
 public class Island {
     private final int width;
     private final int height;
-    private Area[][] areas;
-    public Island(int width,int height){
-        this.width=width;
+    private final Area[][] areas;
+
+    public Island(int width, int height) {
+        this.width = width;
         this.height = height;
         areas = new Area[width][height];
     }
 
+    public void inhabitIsland() {
+        for (int i = 0; i < areas.length; i++) {
+            for (int j = 0; j < areas[i].length; j++) {
+                areas[i][j] = new Area(CreatureFactory.getAnimals(AnimalType.values()),
+                        CreatureFactory.getPlants(PlantType.values()), i, j);
+            }
+        }
+    }
 
+    public Area[][] getAreas() {
+        return areas;
+    }
 }
